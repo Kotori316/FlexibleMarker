2ed153ba673b3cb5e5123a7a29e211ef00d39566
Kotori316 <Kotori316@gmail.com>
2020-02-26 16:33:30 +0900

    Added texture of controller.
    (cherry picked from commit 0f2eebb357ade68832a7387a01a4d6aa4a313e60)

--- src/main/java/com/kotori316/marker/RemoteControlItem.java
+++ src/main/java/com/kotori316/marker/RemoteControlItem.java
@@ -95,6 +95,9 @@ public class RemoteControlItem extends Item {
                 if (context.isPlacerSneaking() && stack.hasTag()) {
                     stack.removeChildTag(NBT_REMOTE_POS);
                     stack.removeChildTag(NBT_AREA);
+                    LOGGER.debug("Reset controller setting.");
+                    Optional.ofNullable(context.getPlayer()).ifPresent(p ->
+                        p.sendStatusMessage(new TranslationTextComponent("chat.flexiblemarker.reset"), false));
                     return ActionResultType.SUCCESS;
                 } else {
                     return super.onItemUseFirst(stack, context);

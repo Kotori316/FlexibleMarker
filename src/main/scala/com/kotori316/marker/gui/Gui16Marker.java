efb345b320a6e780321da335076f331e8104b0f5
Kotori316 <Kotori316@gmail.com>
2022-11-27 23:56:53 +0900

    Press Ctrl or Shift to increase more range

--- src/main/scala/com/kotori316/marker/gui/Gui16Marker.java
+++ src/main/scala/com/kotori316/marker/gui/Gui16Marker.java
@@ -52,6 +52,13 @@ public class Gui16Marker extends GuiContainer {
         String yMinText = Integer.toString(marker.min().getY());
         this.fontRenderer.drawString(yMaxText, this.xSize / 2 + 10 + BUTTON_WIDTH - this.fontRenderer.getStringWidth(yMaxText) / 2, 15 + 23, 0x404040);
         this.fontRenderer.drawString(yMinText, this.xSize / 2 - 10 - BUTTON_WIDTH - this.fontRenderer.getStringWidth(yMinText) / 2, 15 + 23, 0x404040);
+
+        for (GuiButton button : this.buttonList) {
+            if (button.isMouseOver()) {
+                button.drawButtonForegroundLayer(mouseX - this.guiLeft, mouseY - this.guiTop);
+                break;
+            }
+        }
     }
 
     @Override
@@ -61,10 +68,10 @@ public class Gui16Marker extends GuiContainer {
         final int middle = guiLeft + this.xSize / 2;
         this.buttonList.add(new GuiButton(0, middle - BUTTON_WIDTH / 2, guiTop + tp, BUTTON_WIDTH, 20, "+"));
         this.buttonList.add(new GuiButton(1, middle - BUTTON_WIDTH / 2, guiTop + tp + 33, BUTTON_WIDTH, 20, "-"));
-        this.buttonList.add(new GuiButton(2, middle + BUTTON_WIDTH / 2 + 10, guiTop + tp, BUTTON_WIDTH, 20, "Top+"));
-        this.buttonList.add(new GuiButton(3, middle + BUTTON_WIDTH / 2 + 10, guiTop + tp + 33, BUTTON_WIDTH, 20, "Top-"));
-        this.buttonList.add(new GuiButton(4, middle - BUTTON_WIDTH / 2 - 10 - BUTTON_WIDTH, guiTop + tp, BUTTON_WIDTH, 20, "Bottom+"));
-        this.buttonList.add(new GuiButton(5, middle - BUTTON_WIDTH / 2 - 10 - BUTTON_WIDTH, guiTop + tp + 33, BUTTON_WIDTH, 20, "Bottom-"));
+        this.buttonList.add(new YChangeButton(2, middle + BUTTON_WIDTH / 2 + 10, guiTop + tp, BUTTON_WIDTH, 20, "Top+"));
+        this.buttonList.add(new YChangeButton(3, middle + BUTTON_WIDTH / 2 + 10, guiTop + tp + 33, BUTTON_WIDTH, 20, "Top-"));
+        this.buttonList.add(new YChangeButton(4, middle - BUTTON_WIDTH / 2 - 10 - BUTTON_WIDTH, guiTop + tp, BUTTON_WIDTH, 20, "Bottom+"));
+        this.buttonList.add(new YChangeButton(5, middle - BUTTON_WIDTH / 2 - 10 - BUTTON_WIDTH, guiTop + tp + 33, BUTTON_WIDTH, 20, "Bottom-"));
     }
 
     @Override
@@ -72,6 +79,16 @@ public class Gui16Marker extends GuiContainer {
         super.actionPerformed(button);
         int size = marker.getSize();
         int yMin = marker.min().getY(), yMax = marker.max().getY();
+        int n;
+        if (isShiftKeyDown() && isCtrlKeyDown()) { // Shift + Ctrl
+            n = 64;
+        } else if (isShiftKeyDown()) { // Shift
+            n = 16;
+        } else if (isCtrlKeyDown()) { // Ctrl
+            n = 4;
+        } else {
+            n = 1;
+        }
         switch (button.id) {
             case 0: // Plus
                 size = marker.getSize() + CHUNK;
@@ -84,18 +101,30 @@ public class Gui16Marker extends GuiContainer {
                 }
                 break;
             case 2:
-                yMax = marker.max().getY() + 1;
+                yMax = marker.max().getY() + n;
                 break;
             case 3:
-                yMax = Math.max(marker.max().getY() - 1, yMin);
+                yMax = Math.max(marker.max().getY() - n, yMin);
                 break;
             case 4:
-                yMin = Math.min(marker.min().getY() + 1, yMax);
+                yMin = Math.min(marker.min().getY() + n, yMax);
                 break;
             case 5:
-                yMin = Math.max(marker.min().getY() - 1, 0);
+                yMin = Math.max(marker.min().getY() - n, 0);
                 break;
         }
         PacketHandler.sendToServer(new Button16Message(marker.getPos(), marker.getWorld().provider.getDimension(), size, yMax, yMin));
+    }
+
+    private class YChangeButton extends GuiButton {
+        public YChangeButton(int index, int x, int y, int width, int height, String text) {
+            super(index, x, y, width, height, text);
+        }
+
+        @Override
+        public void drawButtonForegroundLayer(int x, int y) {
+            super.drawButtonForegroundLayer(x, y);
+            Gui16Marker.this.drawHoveringText("Ctrl: 4, Shift: 16, Ctrl+Shift: 64", x, y);
+        }
     }
 }
